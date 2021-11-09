5402f78250e967b3322318a9b447b50714bec9f7
vhanla <vhanla@gmail.com>
2021-11-09 18:44:12 -0500

    Improved hotkey hooks commands.

--- AltTabPro.dpr
+++ AltTabPro.dpr
@@ -2,13 +2,14 @@ program AltTabPro;
 
 uses
   Vcl.Forms,
-  main in 'main.pas' {frmAltTabPro};
+  main in 'main.pas' {frmAltTabPro},
+  skins in 'skins.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.MainFormOnTaskbar := False;
+//  Application.MainFormOnTaskbar := False;
   Application.ShowMainForm := False;
   Application.CreateForm(TfrmAltTabPro, frmAltTabPro);
   Application.Run;
